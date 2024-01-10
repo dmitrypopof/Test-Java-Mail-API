1edb7165cf55ca9838dc7a10b9649f8366c83dc5
d.popov <dmitrypopof@mail.ru>
2024-01-10 23:25:23 +0300

    1.2 version

--- src/test/java/org/example/TestMailYandex.java
+++ src/test/java/org/example/TestMailYandex.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 
 import javax.mail.*;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
@@ -14,35 +13,49 @@ public class TestMailYandex extends TestBase{
         FileInputStream fileInputStream = new FileInputStream("config.properties");
         Properties properties = new Properties();
         properties.load(fileInputStream);
-
         String  user = properties.getProperty("mail.user");
-        System.out.println(user);
         String password = properties.getProperty("mail.password");
-        System.out.println(password);
         String host = properties.getProperty("mail.host");
-        System.out.println(host);
-
+        //Настройка почтового сервера:
         Properties prop = new Properties();
         prop.put("mail.store.protocol","imaps");//SSL
-        //СЃРѕР·РґР°С‚СЊ С…СЂР°РЅРёР»РёС‰Рµ СЃРѕРѕР±С‰РµРЅРёР№:
+        //создать хранилище сообщений:
         Store store = Session.getInstance(prop).getStore();
-        store.connect(host,user, password);// РїРѕРґРєР»СЋС‡РµРЅРёРµ С…СЂР°РЅРёР»РёС‰Р°
-        Folder inbox = store.getFolder("INBOX");// РџРђРџРљРђ Р’РҐРћР”РЇР©РР•
-        inbox.open(Folder.READ_WRITE);
+        store.connect(host,user, password);// подключение хранилища
+        //открытие почтового ящика и получение сообщений
+        Folder inbox = store.getFolder("INBOX");// создание папки ВХОДЯЩИЕ
+        inbox.open(Folder.READ_ONLY);
 
-        System.out.println("РљРѕР»РёС‡РµСЃС‚РІРѕ РїРёСЃРµРј: " + inbox.getMessageCount());
+        System.out.println("Количество писем: " + inbox.getMessageCount());
 
-        //РџРѕРёСЃРє РїРѕСЃР»РµРґРЅРµРіРѕ СЃРѕРѕР±С‰РµРЅРёСЏ
+        //Поиск последнего сообщения
         Message message = inbox.getMessage(inbox.getMessageCount());
-
-        Multipart multipart = (Multipart) message.getContent(); // СЃРѕС…СЂР°РЅРµРЅРёРµ РїРёСЃСЊРјР° РІ СЃС‚СЂСѓРєС‚СѓСЂРёСЂРѕРІР°РЅРЅС‹Р№ РІРёРґ
+        Multipart multipart = (Multipart) message.getContent(); // сохранение письма в структурированный вид
         System.out.println(multipart.getContentType());
-
         BodyPart body = multipart.getBodyPart(0);
         System.out.println(body.getContent());
 
-String textActual = body.getContent().toString();
-        System.out.println("РўРµРєСЃС‚ СЃРѕРѕР±С‰РµРЅРёСЏ: " + textActual);
+        //поиск определенного сообщения:
+        Message[] messages = inbox.getMessages();
+        String subjectToFind = "Пароль ГГИС изменен!";//тема сообщения
+        Message foundMessage = null;
+        for(Message message1: messages) {
+            if(message1.getSubject().equals(subjectToFind)){
+                foundMessage = message1;
+                break;
+            }
+        }
+
+        //Проверка, найдено ли сообщение
+        if(foundMessage!=null){
+            System.out.println("Сообщение найдено");
+        } else {
+            System.out.println("Сообщение не найдено");
+        }
+
+        inbox.close(false);
+        store.close();
+
 
     }
 }
