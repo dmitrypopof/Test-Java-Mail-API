d0c0a34ef6327e44c0df1c14c950b7f0b2cee642
d.popov <dmitrypopof@mail.ru>
2024-01-17 22:23:11 +0300

    1.4 version. Чтение входящих сообщений

--- src/test/java/org/example/TestMailRU.java
+++ src/test/java/org/example/TestMailRU.java
@@ -7,90 +7,10 @@ import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMultipart;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
-import static org.example.ReadEmail.getTextFromMimeMultipart;
-
-public class TestMailRU extends TestBase{
-    @Test
-    public void checkWriteInbox() throws IOException, MessagingException {
-        FileInputStream fileInputStream = new FileInputStream("config.properties");
-        Properties properties = new Properties();
-        properties.load(fileInputStream);
-        String  user = properties.getProperty("mail.user");
-        String password = properties.getProperty("mail.password");
-        String host = properties.getProperty("mail.host");
-        //Настройка почтового сервера:
-        Properties prop = new Properties();
-        prop.put("mail.store.protocol","imaps");//SSL
-        //создать хранилище сообщений:
-        Store store = Session.getInstance(prop).getStore();
-        store.connect(host,user, password);// подключение хранилища
-        //открытие почтового ящика и получение сообщений
-        Folder inbox = store.getFolder("INBOX");// создание папки ВХОДЯЩИЕ
-        inbox.open(Folder.READ_ONLY);
-
-        System.out.println("Count message: " + inbox.getMessageCount());
-
-        //Поиск последнего сообщения
-        Message message = inbox.getMessage(inbox.getMessageCount());
-        Multipart multipart = (Multipart) message.getContent(); // сохранение письма в структурированный вид
-        BodyPart bodyPart = multipart.getBodyPart(0);
-        System.out.println(bodyPart.getContent().toString());
-        String content = bodyPart.getContent().toString();
-        System.out.println(content);
-//        System.out.println(multipart.getContentType());
-//        BodyPart body = multipart.getBodyPart(1);
-//        System.out.println(body.getContent());
-
-        //поиск определенного сообщения:
-        Message[] messages = inbox.getMessages();
-//        String subjectToFind = "Пароль ГГИС изменен!";//тема сообщения
-//        Message foundMessage = null;
-//        for(Message message1: messages) {
-//            if(message1.getSubject().equals(subjectToFind)){
-//                foundMessage = message1;
-//                break;
-//            }
-//        }
-
-        //Проверка, найдено ли сообщение
-//        if(foundMessage!=null){
-//            System.out.println("Message found! ");
-//        } else {
-//            System.out.println("Message not found! ");
-//        }
-
-
-
-
-
-        if(messages.length > 0){
-            Message lastMessage = messages[messages.length-1];
-            String textTheme = lastMessage.getSubject().toString(); // тема письма
-            System.out.println("Theme: "+textTheme);
-
-            String textContentType = lastMessage.getContentType().toString();
-            System.out.println(textContentType);
-
-            System.out.println(lastMessage.getSentDate().toString()); // дата получения
-
-            System.out.println(lastMessage.getFrom()[0].toString()); // отправитель
-
-           // System.out.println((MimeMessage) lastMessage).getContent(); // отправитель
-
-        }
-
-
-
-        inbox.close(false);
-        store.close();
-
-
-    }
-
+public class TestMailRU {
     @Test
     public void testLastMail() throws IOException {
         FileInputStream fileInputStream = new FileInputStream("config.properties");
@@ -123,24 +43,33 @@ public class TestMailRU extends TestBase{
             Message[] messages = inbox.getMessages();
             Message lastMessage = messages[messages.length - 1];
 
-            System.out.println("Theme: "+ lastMessage.getSubject());
+            //Тема последнего сообщения:
+            System.out.println("Subject:\n"+ lastMessage.getSubject());
 
-            MimeMultipart multipart = (MimeMultipart) lastMessage.getContent();
-            int count = multipart.getCount();// узнать количество частей контента
-            System.out.println(count);
-            BodyPart bodyPart = multipart.getBodyPart(0); // узнать количество частей контента
-
-
-
-
-            System.out.println("Theme: " + lastMessage.getSubject());
+            //Преобразование последнего сообщения в String c помощью метода getTextFromMimeMultipart
             String content = getTextFromMimeMultipart((Multipart) lastMessage.getContent());
-            System.out.println("Content: " + content);
+            System.out.println("Content:\n" + content);
             // Закрытие соединения
             inbox.close(false);
             store.close();
         }catch (Exception e){e.printStackTrace();}
     }
+
+    //Преобразование последнего сообщения в String:
+    public static String getTextFromMimeMultipart(Multipart mimeMultipart) throws MessagingException, IOException {
+        int count = mimeMultipart.getCount();
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < count; i++) {
+            BodyPart bodyPart = mimeMultipart.getBodyPart(i);
+            if (bodyPart.isMimeType("text/plain")) {
+                result.append(bodyPart.getContent());
+            } else if (bodyPart.isMimeType("multipart/*")) {
+                result.append(getTextFromMimeMultipart((Multipart) bodyPart.getContent()));
+            }
+        }
+        return result.toString();
+    }
+
 }
 
 
